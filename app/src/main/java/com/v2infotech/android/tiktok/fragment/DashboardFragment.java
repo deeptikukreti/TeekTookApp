1abc0e7b21bc786b0592586d7bd00bdd721d5e79
deepti <deeptikukreti98@gmail.com>
2019-02-25 10:54:38 +0530

    new change code

--- app/src/main/java/com/v2infotech/android/tiktok/fragment/DashboardFragment.java
+++ app/src/main/java/com/v2infotech/android/tiktok/fragment/DashboardFragment.java
@@ -1,21 +1,23 @@
 package com.v2infotech.android.tiktok.fragment;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.v2infotech.android.tiktok.R;
+import com.v2infotech.android.tiktok.adapter.TabAdapter;
 
 
 public class DashboardFragment extends Fragment {
 
-    public DashboardFragment() {
-        // Required empty public constructor
-    }
-
-
+    private TabAdapter adapter;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,7 +29,19 @@ public class DashboardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+        view = inflater.inflate(R.layout.fragment_dashboard, container, false);
+
+        viewPager = (ViewPager) view.findViewById(R.id.viewPager);
+        tabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
+        adapter = new TabAdapter(getActivity().getSupportFragmentManager());
+        adapter.addFragment(new TabFragment1(), "For You");
+        adapter.addFragment(new TabFragment2(), "Following");
+
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+        return view;
     }
 
 
