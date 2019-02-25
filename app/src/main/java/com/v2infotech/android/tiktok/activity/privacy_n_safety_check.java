ebb643365394c7d8ad001efa66f6bd9a178a77dc
deepti <deeptikukreti98@gmail.com>
2019-02-25 18:23:39 +0530

    new change code123

---
+++ app/src/main/java/com/v2infotech/android/tiktok/activity/privacy_n_safety_check.java
@@ -0,0 +1,15 @@
+package com.v2infotech.android.tiktok.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.v2infotech.android.tiktok.R;
+
+public class privacy_n_safety_check extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_n_safety_check);
+    }
+}
---
+++ app/src/main/java/com/v2infotech/android/tiktok/adapter/SelectPrivacyOptionAdapter.java
@@ -0,0 +1,4 @@
+package com.v2infotech.android.tiktok.adapter;
+
+public class SelectPrivacyOptionAdapter {
+}
